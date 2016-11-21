d4204fb3a055a339980c5b063a47a62c7b75d145
Artur Kraev <ravenox@gmail.com>
2016-11-21 22:00:57 +0300

    fix build.cmd

--- build.cmd
+++ build.cmd
@@ -1,13 +1,10 @@
-md %~dp0artifacts
+@echo off
+setlocal
 
-%~dp0util\nuget.exe restore %~dp0src\RabbitLink.sln
+md "%~dp0"
+
+dotnet pack .\src\RabbitLink -o artifacts\
 
 @if %errorlevel% neq 0 ( pause & exit /b %errorlevel%)
-
-"C:\Program Files (x86)\MSBuild\14.0\bin\msbuild.exe" /m /nologo "/consoleloggerparameters:PerformanceSummary;Summary" /verbosity:minimal /p:Configuration=Release "src\RabbitLink.sln"
-
-@if %errorlevel% neq 0 ( pause & exit /b %errorlevel%)
-
-%~dp0util\nuget.exe pack %~dp0src\RabbitLink\RabbitLink.csproj -OutputDirectory %~dp0artifacts -Properties Configuration=Release
 
 pause
