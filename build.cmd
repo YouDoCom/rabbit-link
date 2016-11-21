06545c5b97dc63bb9011a2ca1ba5bf1e2632df1c
Artur Kraev <ravenox@gmail.com>
2016-11-21 22:03:25 +0300

    update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,10 +1,14 @@
 @echo off
 setlocal
 
-md "%~dp0"
+cd "%~dp0"
 
+echo Restoring packages
+dotnet restore .\src\RabbitLink
+@if %errorlevel% neq 0 ( pause & exit /b %errorlevel%)
+
+echo Building package
 dotnet pack .\src\RabbitLink -o artifacts\
-
 @if %errorlevel% neq 0 ( pause & exit /b %errorlevel%)
 
 pause
